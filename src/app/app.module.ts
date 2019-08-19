672be956d0dcb8711683fdc2ff7065d3be370f52
Pablo Sousa <pablo.ufopa@gmail.com>
2019-08-19 17:21:48 -0300

    Aula #01 - Criando a parte visual e interativa do app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Keyboard } from '@ionic-native/keyboard/ngx'
 
 @NgModule({
   declarations: [AppComponent],
